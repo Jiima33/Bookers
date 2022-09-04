4d5f9edd2b3023afb85407062ed3ff19f4acafce
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-09-04 12:22:39 +0000

    [add]新規登録機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,26 +1,24 @@
 class BooksController < ApplicationController
+  
+  def index
+    @books = Book.all
+  end
+  
   def new
     @book = Book.new
-  end
+  end 
   
   def create
     book = Book.new(book_params)
     book.save
     redirect_to '/books'
   end
-
-  def index
-  end
-
-  def show
-  end
-
-  def edit
-  end
   
   private
-
+  
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title, :body)
   end
-end
+
+  
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
   root to:'homes#top'
-  resources :books
+  get '/books' => 'books#index'
+  post 'books' => 'books#create'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
