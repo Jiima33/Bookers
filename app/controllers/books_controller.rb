d5c5cafebb8e92a7c69274f972ae481b0636f1ea
SakiOjima <100123445+Jiima33@users.noreply.github.com>
2022-09-05 07:56:42 +0000

    [add]チェックリストのフラッシュメッセージの実装まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,22 +2,39 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
-  def new
-    @book = Book.new
-  end 
-  
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id), flash: { notice: 'Book was successfully created.' }
+  end
+  
+  def show
+    @book = Book.find(params[:id])
+  end
+  
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+   book = Book.find(params[:id])
+   book.update(book_params)
+   redirect_to book_path(book.id), flash: { notice: 'Book was successfully updated.' }
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
   
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
   
